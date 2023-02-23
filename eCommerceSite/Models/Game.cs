ad51390e75baa6ff5cdc16ebe467ed3cf1c66281
Armiger21 <Porfirdruid1@gmail.com>
2023-02-23 00:56:56 -0800

    Fixed Game.cs

--- eCommerceSite/Models/Game.cs
+++ eCommerceSite/Models/Game.cs
@@ -1,7 +1,21 @@
-﻿namespace eCommerceSite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceSite.Models
 {
     public class Game
     {
+        [Key]
         public int GameId { get; set; }
+
+        /// <summary>
+        /// The official title of the video game
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+        /// <summary>
+        /// The sales price of the game
+        /// </summary>
+        [Range(0, 1000)]
+        public double Price { get; set; }
     }
 }
