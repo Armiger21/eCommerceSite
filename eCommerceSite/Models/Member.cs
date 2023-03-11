949e2a1d4bcc65ac8e712e9e606dde5e17311a62
Porfir21 <104858197+Porfir21@users.noreply.github.com>
2023-03-11 13:50:39 -0800

    Added Migration for member class

--- eCommerceSite/Models/Member.cs
+++ eCommerceSite/Models/Member.cs
@@ -1,6 +1,20 @@
-﻿namespace eCommerceSite.Models
+﻿using MessagePack;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceSite.Models
 {
     public class Member
     {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int MemberId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Username { get; set; }
+
+        public string Phone { get; set; }
+
     }
 }
