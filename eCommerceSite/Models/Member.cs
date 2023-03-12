75756cf07420a42befdcfb10f0a34409598c7d77
Porfir21 <104858197+Porfir21@users.noreply.github.com>
2023-03-11 17:40:39 -0800

    Added register view

--- eCommerceSite/Models/Member.cs
+++ eCommerceSite/Models/Member.cs
@@ -20,6 +20,24 @@ namespace eCommerceSite.Models
 
     public class RegisterViewModel
     {
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
 
+        [Required]
+        [StringLength(75, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof (Password))]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
     }
 }
