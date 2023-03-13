593e7ceeef9e6daf143ad599c94b55b625e4a770
Porfir21 <104858197+Porfir21@users.noreply.github.com>
2023-03-13 14:25:54 -0800

    Username

--- eCommerceSite/Controllers/MembersController.cs
+++ eCommerceSite/Controllers/MembersController.cs
@@ -20,7 +20,7 @@ namespace eCommerceSite.Controllers
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel regModel) 
+        public async Task<IActionResult> Register(RegisterViewModel regModel)
         {
             if (ModelState.IsValid)
             {
@@ -33,9 +33,53 @@ namespace eCommerceSite.Controllers
                 _context.Members.Add(newMember);
                 await _context.SaveChangesAsync();
 
+                LogUserIn(newMember.Email);
+                // Redirect to home page
                 return RedirectToAction("Index", "Home");
             }
             return View(regModel);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // check db for credentials
+                Member m = (from member in _context.Members
+                            where member.Email == loginModel.Email &&
+                                  member.Password == loginModel.Password
+                            select member).SingleOrDefault();
+
+                // If exists, send to homepage
+                if (m != null)
+                {
+                    LogUserIn(loginModel.Email);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Credentials not found!");
+            }
+            // Return page if no record found, or ModelState is invalid
+            return View(loginModel);
+
+        }
+
+        private void LogUserIn(string email)
+        {
+            HttpContext.Session.SetString("Email", email);
+        }
+
+        public IActionResult logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
